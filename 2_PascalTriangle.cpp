34fd6f0082d81da728c034af8c2dd2a61b6b9084
Pratishtha saini <103769329+pratishthasaini@users.noreply.github.com>
2024-06-04 00:35:42 +0530

    Update 2_PascalTriangle.cpp

--- 2_PascalTriangle.cpp
+++ 2_PascalTriangle.cpp
@@ -4,10 +4,10 @@ public:
         vector<vector<int>> ans(numRows);
         for(int i = 0; i < numRows; i++){
             ans[i].resize(i+1);
-            for(int j = 0; j < i; j++){               
-                if(j == 0 || j == ans[i].size()-1 ){ ans[i][j] = 1; }
-                ans[i][j] = ans[i-1][j-1]+ans[i-1][j];
-                
+            for(int j = 0; j <= i; j++){               
+                if(j == 0 || j == i ){ ans[i][j] = 1; }
+                else {ans[i][j] = ans[i-1][j-1]+ans[i-1][j];
+                }
             }
         }
         return ans;
