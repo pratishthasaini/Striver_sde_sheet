c3aff0280ca2fe7a61ce28e1877faade72f79ada
Pratishtha saini <103769329+pratishthasaini@users.noreply.github.com>
2024-06-20 23:37:00 +0530

    Update 17_uniquePaths.cpp

--- 17_uniquePaths.cpp
+++ 17_uniquePaths.cpp
@@ -16,3 +16,19 @@ public:
         return dp[0][0];
     }
 };
+
+// or most optimal
+
+class Solution {
+public:
+ 
+    int uniquePaths(int m, int n) {
+        int N = n + m - 2;
+        int r = m - 1; 
+        double res = 1;
+        
+        for (int i = 1; i <= r; i++)
+            res = res * (N - r + i) / i;
+        return (int)res;
+    }
+};
