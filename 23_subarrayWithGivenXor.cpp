5de27de57ed9d5b4d1c77175ff4799fea8acd09d
Pratishtha saini <103769329+pratishthasaini@users.noreply.github.com>
2024-08-08 21:07:24 +0530

    Create 23_subarrayWithGivenXor.cpp

---
+++ 23_subarrayWithGivenXor.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+int subarraysWithSumK(vector < int > a, int k) {  
+    int xr = 0;
+    map<int,int> mp;
+    mp[xr]++;
+    int cnt = 0;
+    for (int i = 0; i < a.size(); i++) {
+       xr = xr ^ a[i];
+       int x = xr ^ k;
+       cnt += mp[x];
+       mp[xr]++;
+     }
+    return cnt;
+}
