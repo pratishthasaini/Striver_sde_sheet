3c9e0146fb06c0c2b26cbb4b488c97b2d70bd607
Pratishtha saini <103769329+pratishthasaini@users.noreply.github.com>
2024-08-08 21:45:02 +0530

    Create 25_ReverseLinkedList.cpp

---
+++ 25_ReverseLinkedList.cpp
@@ -0,0 +1,41 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+        
+
+*****************************************************************/
+
+Node* reverseLinkedList(Node *head)
+{
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp != NULL){
+        Node* front = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = front;
+    }
+    return prev;
+}
